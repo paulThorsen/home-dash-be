8698a86a4abdd19d40a869d66ccc45a7db5bf920
Paul Thorsen <paul_thorsen@byu.edu>
2023-11-10 12:55:23 -0700

    add logout on server shutdown

--- services/meross.js
+++ services/meross.js
@@ -4,7 +4,7 @@ const MerossCloud = require('meross-cloud');
 const options = {
     email: process.env.MEROSS_EMAIL,
     password: process.env.MEROSS_PASSWORD,
-    // logger: console.log,
+    logger: console.log,
     localHttpFirst: true, // Try to contact the devices locally before trying the cloud
     onlyLocalForGet: true, // When trying locally, do not try the cloud for GET requests at all
     timeout: 3000, // Default is 3000
@@ -13,5 +13,14 @@ const options = {
 const meross = {};
 meross.merossCloud = new MerossCloud(options);
 meross.garageDevice = null;
+
+// Make sure we logout when the server is killed.
+const shutDown = () => {
+    // TODO: test this. The idea is that tokens should be invalidated.
+    meross.merossCloud.logout();
+    process.exit(0);
+};
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
 
 module.exports = meross;
