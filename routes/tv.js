70ccd8417846143dc5101629bd702b2313921301
Paul Thorsen <paul_thorsen@byu.edu>
2023-12-04 12:39:09 -0700

    increase sleep

--- routes/tv.js
+++ routes/tv.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/appleTV', async function (req, res, next) {
     await roku.turnOnTV();
     // Necessary to wait until next command can be supplied.
-    await new Promise((resolve) => setTimeout(resolve, 1500));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     await roku.switchInputToHDMI1();
     res.send();
 });
